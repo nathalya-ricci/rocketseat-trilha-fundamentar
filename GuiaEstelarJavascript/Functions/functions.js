38d90b873b835fc818f307536ed32a7a6f2216b5
Nathálya Ricci <nathalyaricci@gmail.com>
2022-11-02 20:40:55 -0300

    add functions.js

---
+++ GuiaEstelarJavascript/Functions/functions.js
@@ -0,0 +1,15 @@
+// Criar um aplicativo de frases motivacionais
+
+// function statement - declarar a função
+
+function createPhrases() {
+  console.log("Estudar é muito bom");
+	console.log("Paciência e persistência");
+	console.log("Revisão é mãe do aprendizado");
+}
+
+// execute, invoke, call, run - executar a função
+
+createPhrases()
+
+console.log('Fim do programa');
