fbced9d31270cce1c6a28e6680d22fc97afc18ad
Nathálya Ricci <nathalyaricci@gmail.com>
2022-12-16 19:24:48 -0300

    corrige tipo de arquivo

---
+++ GuiaEstelarJavascript/Type conversion vs type coesion/Manipulando Strings e Numbers/contar-caracteres-e-digitos.js
@@ -0,0 +1,9 @@
+// Contar quantos caracteres tem uma palavra e quantos dígitos tem um número
+
+let word = "Paralelepípedo";
+console.log(word.length);
+
+let number = 1234;
+console.log(number.length);
+// Number não recebe length
+console.log(String(number).length);
---
+++ GuiaEstelarJavascript/Type conversion vs type coesion/Manipulando Strings e Numbers/string-to-number-number-to-string.js
@@ -0,0 +1,7 @@
+// Transformar String em Number e Number em String
+
+let string = "123";
+console.log(Number(string));
+
+let number = 321;
+console.log(String(number));
