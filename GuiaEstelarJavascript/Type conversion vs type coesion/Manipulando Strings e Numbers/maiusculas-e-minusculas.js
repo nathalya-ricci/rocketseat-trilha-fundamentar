6bd3b75c37be317afb74611ebf42859e75ce2fcf
Nathálya Ricci <nathalyaricci@gmail.com>
2022-12-16 19:38:47 -0300

    add maiusculas-e-minusculas.js

---
+++ GuiaEstelarJavascript/Type conversion vs type coesion/Manipulando Strings e Numbers/maiusculas-e-minusculas.js
@@ -0,0 +1,5 @@
+// Transforme letras minúsculas em maiúsculas. Faça o contrário também
+
+let palavra = "Quero aprender logo React!";
+console.log(palavra.toLowerCase());
+console.log(palavra.toUpperCase());
