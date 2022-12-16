b28d443c1c967fb17ab6003c15cc17462bf9fef4
Nathálya Ricci <nathalyaricci@gmail.com>
2022-12-16 20:48:52 -0300

    add encontrando-palavras-em-frases.js

---
+++ GuiaEstelarJavascript/Type conversion vs type coesion/Manipulando Strings e Arrays/encontrando-palavras-em-frases.js
@@ -0,0 +1,9 @@
+// Verificar se o texto contém a palavra "Amor"
+
+let frase = "Você não sabe o que é o amor";
+console.log(frase.includes("Amor"));
+// retorna false, pois é case sensitive
+
+let frase2 = "Amor verdadeiro";
+console.log(frase2.includes("Amor"));
+// retona true
