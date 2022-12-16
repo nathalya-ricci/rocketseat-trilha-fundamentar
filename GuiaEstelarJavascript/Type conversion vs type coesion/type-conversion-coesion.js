55d8f3bded26c7842f6a972638fbb4cf0253edf1
Nathálya Ricci <nathalyaricci@gmail.com>
2022-12-16 19:21:11 -0300

    add Type conversion vs type coesion

---
+++ GuiaEstelarJavascript/Type conversion vs type coesion/type-conversion-coesion.js
@@ -0,0 +1,13 @@
+/*
+  Type conversion (typecasting) vs Type coesion
+
+  * Alteração de um tipo de dado para outro tipo
+*/
+
+// type coesion (o próprio js muda)
+console.log("9" + 5);
+// transformou o 5 (number) para string
+
+// type conversion (fazendo manualmente)
+console.log(Number("9") + 5);
+// transformou o o 9 (string) para number
