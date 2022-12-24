8b766af86887bf8e5c98ee7fe7dbf5cd4ebdba9e
Nathálya Ricci <nathalyaricci@gmail.com>
2022-12-24 16:40:03 -0300

    add Expressões e operadores

---
+++ GuiaEstelarJavascript/Expressões e operadores/new.js
@@ -0,0 +1,15 @@
+/*
+  new
+
+  * left-hand-side expression
+  * criar um novo objeto
+*/
+
+// let name = new String("Lya");
+// name.surName = 'Ricci'
+// let age = new Number(23);
+
+// console.log(name.surName, age);
+
+let date = new Date("1999-03-14");
+console.log(date);
